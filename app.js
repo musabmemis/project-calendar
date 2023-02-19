66c39c35473390f9cfe067e2b5b791bf66cf1e70
musabmemis <musabmemis@gmail.com>
2023-02-19 04:16:45 +0300

    js update

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const calendar = document.getElementById('calendar');
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 
 const drawBlankCalendar = () => {
@@ -8,6 +23,7 @@ const drawBlankCalendar = () => {
 
         const dayText = document.createElement('p')
         dayText.classList.add('day-text')
+        dayText.innerText = days[i%7]
 
         const dayNumber = document.createElement('p')
         dayNumber.classList.add('day-number')
@@ -21,6 +37,27 @@ const drawBlankCalendar = () => {
         console.log(day);
         calendar.appendChild(day)
     }
+};
+
+const updateCalendar = (month, year, events) => {
+    const dayElements = document.querySelectorAll('.day')
+
+    const theFirst = new Date();
+    theFirst.setMonth(month)
+    theFirst.setYear(year);
+
+    const theFirstDayOfWeek = theFirst.getDay();
+    const monthName = months[month];
+    const monthWithYear = `${year} - ${month}`;
+    const daysInMonth = new Date(year, month + 1, 0).getDate()
+
+    for (let i = 0; i < dayElements.length; i++) {
+        const day = dayElements[i];
+        const dayText = day.querySelector('.day-text');
+        dayText.innerText = days[i%7];
+    }
+
 }
 
 drawBlankCalendar();
+updateCalendar();
